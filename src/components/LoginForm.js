1b2ce99a537592b5552ab20bdbec7df93d382ade
bflip12 <bflip12@gmail.com>
2017-11-20 15:44:28 -0500

    initial branch commit

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import { } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  state = { email: '', password: '' };
+
   render() {
     return (
       <Card>
-        <CardSection />
-        <CardSection />
+        <CardSection>
+          <Input
+            placeholder='user@gmail.com'
+            label='Email'
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+            style={{ height: 20, width: 100 }}
+          />
+        </CardSection>
+        
+        <CardSection>
+          <Input
+            placeholder='password'
+            label='Password'
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            style={{ height: 20, width: 100 }}
+          />
+        </CardSection>
 
         <CardSection>
           <Button>
